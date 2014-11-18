655f5799fbaa651642639adb4256f41dfbb609be
Ann Cascarano <a.cascarano@gmail.com>
2014-11-18 15:16:21 -0500

    added custom post type and ACF plugin

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -16,9 +16,45 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php while ( have_posts() ) : the_post(); 
+			$service = get_field('services');
+			$client = get_field('client');
+			$link = get_field('site_link');
+			$size = "full";
+			$image_1 = get_field('image_1');
+			$image_2 = get_field('image_2');
+			$image_3 = get_field('image_3');
+		?>
+
+		<article class="case-study">
+			<aside class="case-study-sidebar">
+
+				<h2><?php the_title();?></h2>
+				<h6><?php echo $services; ?></h6>
+				<h6 id="client">Client: <?php echo $client; ?></h6>
+
 				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
+
+				<p><a href="<?php echo $link; ?>">Site Link</a></p>
+			</aside>
+
+			<div class="case-study-images">
+				<?php if($image_1) { 
+					echo wp_get_attachment_image( $image_1, $size );
+				} ?>
+
+				<?php if($image_2) { 
+					echo wp_get_attachment_image( $image_2, $size );
+				} ?>
+
+				<?php if($image_3) { 
+					echo wp_get_attachment_image( $image_3, $size );
+				} ?>
+			</div>
+					
+				<?php endwhile; // end of the loop. ?>
+		</article>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
