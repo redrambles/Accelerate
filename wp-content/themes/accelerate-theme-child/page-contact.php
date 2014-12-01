df6c84b302d2b5a851c978bbe2b402c591dbe11b
Ann Cascarano <a.cascarano@gmail.com>
2014-12-01 12:23:51 -0500

    added a template for the contact page so that I could style the reply to the form - also a few changes in style.css to work with this

---
+++ wp-content/themes/accelerate-theme-child/page-contact.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="site-content">
+	<div id="content" role="main">
+		<div class="page-content">
+
+			<?php $method = $_SERVER['REQUEST_METHOD']; ?> 
+
+			<div class="contact-form"> 
+
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> 
+
+					<?php if ($method == 'POST' ): ?> 
+					<div class="reply-contact">
+					
+						<h2>Thanks for Your Message!</h2>
+
+						<p>We'll get back to you shortly.</p>
+
+					</div>
+
+					<?php else: ?> 
+
+					<h3><?php the_title(); ?></h3> 
+
+					<?php the_content(); ?> 
+
+					<?php endif; ?> 
+
+				<?php endwhile; endif; ?> 
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
+
+
