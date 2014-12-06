4af1bae8685690b2c6b9e762f53ff09b1473288a
Ann Cascarano <a.cascarano@gmail.com>
2014-12-06 13:17:26 -0500

    small typo in  404 page

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -18,7 +18,7 @@ get_header(); ?>
 	<div id="content" role="main">
 		<div class="text_404">
 			<h2>That page.</h2>
-			<h4>She is no where to be found.</h4>
+			<h4>She is nowhere to be found.</h4>
 			<p class="forgive">Forgive Us.</p>
 		</div>
 		<figure class="image_404">
