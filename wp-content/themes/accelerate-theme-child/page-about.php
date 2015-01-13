91ec481abfc428120e818e909a957d2c16016c97
Ann Cascarano <a.cascarano@gmail.com>
2015-01-13 18:28:36 -0500

    cleanup to page-about.php

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -11,35 +11,33 @@
 
 get_header(); ?>
 
+	<?php
+		$services_intro_title = get_field('services_intro_title');
+		$services_intro_text = get_field('services_intro_text');
+		$service_1_title = get_field('service_1_title');
+		$service_1_description = get_field('service_1_desc');
+		$service_1_image = get_field('service_1_image');
+		$service_2_title = get_field('service_2_title');
+		$service_2_description = get_field('service_2_desc');
+		$service_2_image = get_field('service_2_image');
+		$service_3_title = get_field('service_3_title');
+		$service_3_description = get_field('service_3_desc');
+		$service_3_image = get_field('service_3_image');
+		$service_4_title = get_field('service_4_title');
+		$service_4_description = get_field('service_4_desc');
+		$service_4_image = get_field('service_4_image');
+		$about_contact_title = get_field('about_contact_title');
+		$contact_button_text = get_field('contact_button_text');
+		$size = "small";
+	?>
 
-<!-- 	<div id="content" role="main"> -->
-		<?php
-			$services_intro_title = get_field('services_intro_title');
-			$services_intro_text = get_field('services_intro_text');
-			$service_1_title = get_field('service_1_title');
-			$service_1_description = get_field('service_1_desc');
-			$service_1_image = get_field('service_1_image');
-			$service_2_title = get_field('service_2_title');
-			$service_2_description = get_field('service_2_desc');
-			$service_2_image = get_field('service_2_image');
-			$service_3_title = get_field('service_3_title');
-			$service_3_description = get_field('service_3_desc');
-			$service_3_image = get_field('service_3_image');
-			$service_4_title = get_field('service_4_title');
-			$service_4_description = get_field('service_4_desc');
-			$service_4_image = get_field('service_4_image');
-			$about_contact_title = get_field('about_contact_title');
-			$contact_button_text = get_field('contact_button_text');
-			$size = "small";
-		?>
-
-		<section class="hero-about">
-				<?php while ( have_posts() ) : the_post(); ?>
-				<div class="hero-text">
-					<h3><?php the_content(); ?></h3>
-				</div>
-				<?php endwhile; // end of the loop. ?>
-		</section>
+	<section class="hero-about">
+			<?php while ( have_posts() ) : the_post(); ?>
+			<div class="hero-text">
+				<h3><?php the_content(); ?></h3>
+			</div>
+			<?php endwhile; // end of the loop. ?>
+	</section>
 
 	<div class="site-content">
 
