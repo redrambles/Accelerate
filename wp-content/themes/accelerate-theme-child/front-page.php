e2f4c7bda38cd8d0448e9f6b6dc25bf20756f5df
Ann Cascarano <a.cascarano@gmail.com>
2016-01-11 11:04:15 -0500

    cleaned up code for front-page.php

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -21,9 +21,6 @@ get_header(); ?>
 		<!-- </div -->><!-- .site-content -->
 </section><!-- .home-page -->
 
-	<!-- Testing -->
-<?php //print_r($wp_query); exit; ?>
-
 <section class="featured-work">
 	<div class="site-content">
 		<h4>Featured Work</h4>
@@ -57,61 +54,28 @@ get_header(); ?>
 	</div>
 </section>
 
-	<?php //testing a different way to fetch the info from the about page - it works as well but I think below is more clear
-	// and we can't really benefit form using a loop because it's all custom fields - the only advantage is that you don't have
-	// to enter the post ID - you would let the loop figure it out
-	//$args = array (
-	// 		'pagename' => 'about'
-	// 	);
-
-	// $test = new WP_Query($args);
-	// if while ($test-> have_posts() ) : $test->the_post(); 
-	// 	$service_1_title = get_field('service_1_title');
-	// 	echo $service_1_title;
-	// endwhile;
-	?>
-
 <section class="featured-services">
 	<div class="site-content">
 		<h4><a href="<?php echo home_url(); ?>/about">Services</a></h4>
+		<?php $args = array (
+			'post_type' => 'services',
+			'posts_per_page' => 4
+				);
+			$services = new WP_Query($args);?>
+
 		<ul class="homepage-featured-services">
-			<?php
-				// You could also create a new custom query fetching pagename="about" info and then use the variables exactly 
-				$service_1_image = get_field('service_1_image', 47331); 
-				//$service_1_image  = get_post_meta(47331, "service_1_image", true); - This would be the NON ACF dependant method
-				$service_2_image = get_field('service_2_image', 47331);
-				$service_3_image = get_field('service_3_image', 47331);
-				$service_4_image = get_field('service_4_image', 47331);
-				$service_1_title = get_field('service_1_title', 47331);
-				$service_2_title = get_field('service_2_title', 47331);
-				$service_3_title = get_field('service_3_title', 47331);
-				$service_4_title = get_field('service_4_title', 47331);
+			<?php while ( $services->have_posts() ) : $services->the_post(); 
+				$cpt_service_image = get_field('cpt_service_image');
 				$size = "medium";
 			 ?>
 		 	<li class="individual-featured-service">	
 			 	<figure>
-			 		<?php echo wp_get_attachment_image($service_1_image, $size); ?>
+			 		<?php echo wp_get_attachment_image($cpt_service_image, $size); ?>
 			 	</figure>
-				<h5><a href="<?php the_permalink(); ?>"><?php echo $service_1_title; ?></a></h5>
+				<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
 			</li>
-			<li class="individual-featured-service">	
-			 	<figure>
-			 		<?php echo wp_get_attachment_image($service_2_image, $size); ?>
-			 	</figure>
-				<h5><a href="<?php the_permalink(); ?>"><?php echo $service_2_title; ?></a></h5>
-			</li>
-			<li class="individual-featured-service">	
-			 	<figure>
-			 		<?php echo wp_get_attachment_image($service_3_image, $size); ?>
-			 	</figure>
-				<h5><a href="<?php the_permalink(); ?>"><?php echo $service_3_title; ?></a></h5>
-			</li>
-			<li class="individual-featured-service">	
-			 	<figure>
-			 		<?php echo wp_get_attachment_image($service_4_image, $size); ?>
-			 	</figure>
-				<h5><a href="<?php the_permalink(); ?>"><?php echo $service_4_title; ?></a></h5>
-			</li>
+			<?php endwhile; //end the while loop
+			wp_reset_postdata(); ?>
 		</ul>
 	</div>
 </section>
@@ -120,7 +84,6 @@ get_header(); ?>
 	<div class="site-content">
 		<div class="blog-post">
 			<h4>From the Blog</h4>
-			<?php //query_posts('posts_per_page=1'); ?>
 			<?php $args = array (
 					'posts_per_page' => 1
 				);
@@ -131,8 +94,7 @@ get_header(); ?>
                 <?php the_excerpt(); ?>
                 <a href="<?php the_permalink(); ?>" class="read-more-link">Read More <span>&rsaquo;</span></a>
 			<?php endwhile; //end the while loop
-			wp_reset_postdata();
-			//wp_reset_query(); ?> <!-- reset altered query back to the original -->
+			wp_reset_postdata(); ?>
 		</div><!-- .blog-post -->
 
 		<!-- Sidebar to host the twitter module  -->
