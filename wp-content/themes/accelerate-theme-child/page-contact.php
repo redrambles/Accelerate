a3bf62ec8e81273957d637942f0c1648ed77b106
Ann Cascarano <a.cascarano@gmail.com>
2016-05-29 14:54:40 -0400

    cleanup page-contact.php

--- wp-content/themes/accelerate-theme-child/page-contact.php
+++ wp-content/themes/accelerate-theme-child/page-contact.php
@@ -1,15 +1,10 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * Custom Contact Page
  *
  * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
  */
 
 get_header(); ?>
