a5bd616d63a4bdfcfb74fdd408863b92e0fb94f6
Ann Cascarano <a.cascarano@gmail.com>
2016-06-04 21:52:39 -0400

    fix back to posts broken link single.php

--- wp-content/themes/accelerate-theme-child/single.php
+++ wp-content/themes/accelerate-theme-child/single.php
@@ -3,19 +3,18 @@
  * The Template for displaying all single posts
  *
  * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
+ * @subpackage Accelerate
+ * @since Accelerate 1.0
  */
 
 get_header(); ?>
 
-	<!-- This is not part of the original assignment -->
-				
+	<!-- BLOG PAGE -->
 	<section class="blog-page">
 		<div class="site-content">
 			<div class="main-content">
-	
-	<?php
+				
+				<?php
 				// Start the Loop.
 				while ( have_posts() ) : the_post(); ?>
 
@@ -23,36 +22,57 @@ get_header(); ?>
 					<div class="entry-wrap">
 						<header class="entry-header">
 							<div class="entry-meta">
-								<?php //the_meta(); ?>
-								<time class="entry-time"><?php the_time('F j, Y');?></time>
+								<time class="entry-time"><?php the_date(); ?></time>
 							</div>
 							<h2 class="entry-title"><?php the_title(); ?></h2>
 						</header>
-
 						<div class="entry-summary">
 							<?php the_content(); ?>
 						</div>
-						
-						<?php accelerate_theme_child_footer_meta(); ?>
-					
+						<footer class="entry-footer">
+							<div class="entry-meta">
+								<span class="entry-terms author">Written by <?php the_author_posts_link(); ?></span>
+								<span class="entry-terms category">Posted in <?php the_category(', '); ?></span>
+								<span class="entry-terms comments"><?php comments_number( 'No comments yet!', '1 comment', '% comments' ); ?></span>
+							</div>
+						</footer>
 					</div>
 				</article>
-			<?php 	// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif; ?>
 
-			</div> <!-- main-content -->
-		<?php get_sidebar(); ?>
+				<div class="comments-area">
+					<h3 class="comments-title">no comments</h2>
+					<div class="comment-respond">
+						<h3 class="comment-reply-title">Leave a comment</h3>
+						<form action="" method="post" id="commentform" class="comment-form default-form">
+							<div class="form-author">
+								<label for="author">Name</label> 
+								<input id="author" name="author" type="text" value="" aria-required="true">
+							</div>
+							<div class="form-email">
+								<label for="email">Email <span>(hidden)</span></label> 
+								<input id="email" name="email" type="email" value="" aria-required="true">
+							</div>
+							<div class="form-comment">
+								<label for="comment">Your comment</label> 
+								<textarea id="comment" name="comment" rows="8" aria-required="true"></textarea>
+							</div>						
+							<input name="submit" type="submit" id="submit" class="submit" value="Post Comment">
+						</form>
+					</div>
+				</div>
 
-	<!-- END blog page -->
+			</div>
+
+			<?php get_sidebar(); ?>
 
 			<footer class="navigation container">
-				<div class="left">&larr;<a href="<?php echo esc_url( home_url() ); ?>/blog">Back to posts</a></div>
+				<div class="left">&larr;<a href="<?php echo home_url(); ?>/blog">back to posts</a></div>
 			</footer>
 	
+		</div>
+	</section>
+	
 				<?php endwhile; ?>
-			</div> <!-- site-content -->
-		</section>
+
 <?php
 get_footer();
