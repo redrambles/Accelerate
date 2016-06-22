38581460e247cc27111b47d4790d57c047aa371a
Ann Cascarano <a.cascarano@gmail.com>
2016-06-21 21:55:47 -0400

    add comment to test-404 to clarify purpose

--- wp-content/themes/accelerate-theme-child/js/test-404.js
+++ wp-content/themes/accelerate-theme-child/js/test-404.js
@@ -1,4 +1,4 @@
-
+// Here is a quick test to see if this code could successfully target the 404 page only
 jQuery(document).ready(function($) {
 
   $('body').css("background-color","lemonchiffon");
