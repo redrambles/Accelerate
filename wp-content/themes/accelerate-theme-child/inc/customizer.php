3f585efd87fb01cc8eb4fb412210573192c5280d
Ann Cascarano <a.cascarano@gmail.com>
2016-10-07 16:22:16 -0400

    clean up customizer.php for use in footer only

--- wp-content/themes/accelerate-theme-child/inc/customizer.php
+++ wp-content/themes/accelerate-theme-child/inc/customizer.php
@@ -1,44 +1,16 @@
 <?php
-/**************************************************************************
-Add theme customizer controls, settings etc.
-**************************************************************************/
 function accelerate_customize_register( $wp_customize ) {
 	
-	/*******************************************
-	Sections
-	********************************************/
-	
-	// contact details section
-	$wp_customize->add_section( 'accelerate_contact' , array(
-		'title' => __( 'Contact Details', 'accelerate-theme-child')
-	) );
-	
+	/* Sections */
+
 	// footer details section
 	$wp_customize->add_section( 'accelerate_footer' , array(
 		'title' => __( 'Footer Details', 'accelerate-theme-child')
 	) );
 	
-	
-	/********************
-	Define generic controls
-	*********************/
+	/* Define generic controls */
 	
 	// create class to define textarea controls in Customizer
-	class accelerate_Customize_Textarea_Control_Address extends WP_Customize_Control {
-		
-		public $type = 'textarea';
-		public function render_content() {
-			
-			echo '<label>';
-				echo '<span class="customize-control-title">' . esc_html( $this-> label ) . '</span>';
-				echo '<textarea rows="2" style ="width: 100%;"';
-				$this->link();
-				echo '>' . esc_textarea( $this->value() ) . '</textarea>';
-			echo '</label>';
-			
-		}
-	}	
-	
 	class accelerate_Customize_Textarea_Control_Footer extends WP_Customize_Control {
 		
 		public $type = 'textarea';
@@ -54,53 +26,7 @@ function accelerate_customize_register( $wp_customize ) {
 		}
 	}	
 	
-	
-	/*******************************************
-	Contact details in header
-	********************************************/
-
-	// address
-	$wp_customize->add_setting( 'accelerate_address_setting', array (
-		'default' => __( 'Your address', 'accelerate-theme-child' )
-	) );
-	$wp_customize->add_control( new accelerate_Customize_Textarea_Control_Address(
-		$wp_customize,
-		'accelerate_address_setting',
-		array( 
-			'label' => __( 'Address', 'accelerate-theme-child' ),
-			'section' => 'accelerate_contact',
-			'settings' => 'accelerate_address_setting'
-	)));
-	
-	// phone number
-	$wp_customize->add_setting( 'accelerate_telephone_setting', array (
-		'default' => __( 'Your phone number', 'accelerate-theme-child' )
-	) );
-	$wp_customize->add_control( new accelerate_Customize_Textarea_Control_Address(
-		$wp_customize,
-		'accelerate_telephone_setting',
-		array( 
-			'label' => __( 'Phone Number', 'accelerate-theme-child' ),
-			'section' => 'accelerate_contact',
-			'settings' => 'accelerate_telephone_setting'
-	)));
-	
-	// email
-	$wp_customize->add_setting( 'accelerate_email_setting', array (
-		'default' => __( 'Your email address', 'accelerate-theme-child' )
-	) );
-	$wp_customize->add_control( new accelerate_Customize_Textarea_Control_Address(
-		$wp_customize,
-		'accelerate_email_setting',
-		array( 
-			'label' => __( 'Email', 'accelerate-theme-child' ),
-			'section' => 'accelerate_contact',
-			'settings' => 'accelerate_email_setting'
-	)));
-	
-	/*******************************************
-	Contact details in footer
-	********************************************/
+	/* Contact details in footer */
 
 	// footer message
 	$wp_customize->add_setting( 'accelerate_footer_message', array (
@@ -117,39 +43,11 @@ function accelerate_customize_register( $wp_customize ) {
 	
 }
 add_action( 'customize_register', 'accelerate_customize_register' );
-/**********************************************************************
-Add controls / content to theme
-**********************************************************************/
-function accelerate_display_contact_details_in_header() { ?>
-	
-	<address>
-		
-		<p class="address">
-			<?php echo get_theme_mod( 'accelerate_address_setting', 'Your address' ); ?>
-		</p>
-		
-		<p class="tel">
-			<?php echo get_theme_mod( 'accelerate_telephone_setting', 'Your telephone number' ); ?>
-		</p>
-		
-		<?php $email = get_theme_mod( 'accelerate_email_setting', 'Your email address' ); ?>
-		<p class="email">
-			<a href="<?php echo $email; ?>">
-				<?php echo $email; ?>
-			</a>
-		</p>
-	
-	</address>
-	
-<?php }
-add_action( 'accelerate_in_header', 'accelerate_display_contact_details_in_header' );
+
+/* Add to theme */
 
 // Give option to display custom footer details - called in footer.php
-function accelerate_display_footer_details() { ?>
-	
-	<p>
-			<?php echo get_theme_mod( 'accelerate_footer_message', 'Your footer message' ); ?>	
-	</p>
-	
-<?php }
+function accelerate_display_footer_details() { 
+	echo "<p>" .get_theme_mod( 'accelerate_footer_message', 'Your footer message' ) . "</p>";
+}
 add_action( 'accelerate_footer', 'accelerate_display_footer_details' );
