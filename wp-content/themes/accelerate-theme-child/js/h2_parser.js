62e86220331b3a6fa5755e1a4d5fb4f9306e2d96
Ann Cascarano <a.cascarano@gmail.com>
2016-10-14 22:04:31 -0400

    generate list with links to h2 in about page

--- wp-content/themes/accelerate-theme-child/js/h2_parser.js
+++ wp-content/themes/accelerate-theme-child/js/h2_parser.js
@@ -1,35 +1,22 @@
 jQuery(document).ready(function($) {
   
-var $ul = document.createElement('ul'),
-$h2 = ('h2'),
-$intro = $('.about-intro'); // I want to put the list right after this element
-$i = 0;
+  var $ul = document.createElement('ul'),
+  $h2 = ('h2'),
+  $intro = $('.about-intro'); // I want to put the list right after this element
+  $i = 0;
 
-$($ul).addClass("link-list");
-$( $h2 ).each(function(){
-  $(this).attr('id', 'subheading-'+$i);
-  $i++;
-});
+  $($ul).addClass("link-list");
 
-$( $h2 ).each(function() {
-  $( $ul ).append( $('<a href="#'+$(this).attr('id')+'" class="header-link" />', ({text: $(this).text()})) );
-  $help = $($ul).children();
-  $( $help ).innerHTML = "barf";
-});
+  $( $h2 ).each(function(){
+    $(this).attr('id', 'subheading-'+$i);
+    $i++;
+  });
 
-$( $ul.children ).each(function() {
-  console.log( $(this).attr('href'));
-  $link = $(".header-link").children();
-  $link.html("fjakslfjkasjf");
-  console.log($link);
-  //  console.log( $(this).children());
-});
+  $( $h2 ).each(function() {
+    var $headerText = $(this).text();
+    $( $ul ).append( $('<a href="#'+$(this).attr('id')+'" class="header-link" >' + $headerText +'</a>' ) );
+  });
 
-//console.log($ul.children);
-// console.log($help);
-
-console.log($ul);
-$($ul).appendTo($intro);
-$('body').append($ul);
+  $( $intro ).append($ul);
 
 })
