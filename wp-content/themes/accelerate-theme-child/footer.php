c5f5e3234a54a4a080fd86da567223068d153605
Ann Cascarano <a.cascarano@gmail.com>
2016-11-22 11:30:58 -0500

    update ACF plugins

--- wp-content/themes/accelerate-theme-child/footer.php
+++ wp-content/themes/accelerate-theme-child/footer.php
@@ -27,8 +27,9 @@
 				</div>
 				<?php if ( has_nav_menu ( 'social-media' ) ) { ?>
 					<nav class="social-media-navigation" role="navigation">
-						<?php wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu', 'link_before'     => '<span class="screen-reader-text">',
-						'link_after'      => '</span>', ) ); ?>
+						<?php wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu', //'link_before'     => '<span class="screen-reader-text">',
+						//'link_after'      => '</span>', 
+					) ); ?>
 					</nav>
 				<?php } ?>
 	
