c6d4c41ca7b5821d2ba1d89f8a64d018b75dcfd3
Ann Cascarano <a.cascarano@gmail.com>
2017-01-10 17:09:19 -0500

    cleaned up footer area for sitename and info

--- wp-content/themes/accelerate-theme-child/footer.php
+++ wp-content/themes/accelerate-theme-child/footer.php
@@ -16,15 +16,15 @@
 			<div class="site-info">
 
 				<div class="site-description">
-					 <!-- <a href="<?php //echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php //bloginfo( 'name' ); ?></a> -->
 					<?php if (!empty(get_theme_mod('accelerate_footer_message'))){ 
 								do_action('accelerate_footer_customizer'); 
 					  } else {
 								do_action('modified_footer'); ?>
-						<p class="footer-desc"><span class="main-color"><?php bloginfo( 'name' ); ?></span> <?php bloginfo('description'); ?></p>
+						<p class="footer-desc"><a href="<?php echo home_url(); ?>"><span class="main-color"><?php bloginfo( 'name' ); ?></span> <?php bloginfo('description'); ?></p></a></p>
 					<?php } ?>
 					<p class="footer-copy">&copy; <?php echo date("Y"); ?> <?php bloginfo('title'); ?>, LLC </p>
 				</div>
+				
 				<?php if ( has_nav_menu ( 'social-media' ) ) { ?>
 					<nav class="social-media-navigation" role="navigation">
 						<?php wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu', //'link_before'     => '<span class="screen-reader-text">',
