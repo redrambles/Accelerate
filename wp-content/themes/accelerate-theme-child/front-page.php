ed34a443e85465d6685d5245b49773a65717f630
Ann Cascarano <a.cascarano@gmail.com>
2017-03-25 13:43:43 -0400

    adding links to services on front page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -83,10 +83,12 @@ get_header(); ?>
 				$size = "medium";
 			 ?>
 		 	<li class="individual-featured-service">	
-			 	<figure>
-			 		<?php echo wp_get_attachment_image($cpt_service_image, $size); ?>
-			 	</figure>
-				<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+			 <a href="<?php the_permalink(); ?>">
+				 	<figure>
+				 		<?php echo wp_get_attachment_image($cpt_service_image, $size); ?>
+				 	</figure>
+					<h5><?php the_title(); ?></h5>
+				</a>
 			</li>
 			<?php endwhile; //end the while loop
 			wp_reset_postdata(); ?>
