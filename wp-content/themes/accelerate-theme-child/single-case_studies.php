e87f320a00e2e88781d30570f5c30c0699546d09
Ann Cascarano <a.cascarano@gmail.com>
2017-04-22 15:29:59 -0400

    small typo fix

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -5,7 +5,7 @@
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site will use a
- * differenat template.
+ * different template.
  *
  * @package WordPress
  * @subpackage Accelerate Marketing
