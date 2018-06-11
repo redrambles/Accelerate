c41883ee6fa426f6edd746d389759bd1aafedc7b
Ann Cascarano <a.cascarano@gmail.com>
2018-06-11 09:24:25 -0400

    Dynamic archive page link for front page button

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -26,7 +26,7 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<div class='homepage-hero'>
 					<?php the_content(); ?>
-					<a class="button" href="<?php echo home_url(); ?>/case-studies">View Our Work</a>			
+					<a class="button" href="<?php echo get_post_type_archive_link('case_studies'); ?>">View Our Work</a>			
 				</div>
 			<?php endwhile; // end of the loop. ?>
 		<!-- </div --><!-- .site-content -->
