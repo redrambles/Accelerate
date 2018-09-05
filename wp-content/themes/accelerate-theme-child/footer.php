fe3e1dbc40ab5bc20e890f332bce8713340edc59
Ann Cascarano <a.cascarano@gmail.com>
2018-09-04 21:05:04 -0400

    trying out manual social icon nav solution from student

--- wp-content/themes/accelerate-theme-child/footer.php
+++ wp-content/themes/accelerate-theme-child/footer.php
@@ -29,9 +29,7 @@
 				
 				<?php if ( has_nav_menu ( 'social-media' ) ) { ?>
 					<nav class="social-media-navigation" role="navigation">
-						<?php wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu','link_before'=>'<alt="test">'
-						//'link_before'     => '<span class="screen-reader-text">',
-						//'link_after'      => '</span>', 
+						<?php  wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu', 'link_before'     => '<span class="screen-reader-text">', 'link_after' => '</span>', 
 					) ); ?>
 					</nav>
 				<?php } ?>
